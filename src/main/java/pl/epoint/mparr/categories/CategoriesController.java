e4fb5b3c22d3976e840784847d428db9200cec69
Marek Parr <mparr@e-point.pl>
2019-09-11 13:37:52 +0200

    poprawki

--- src/main/java/pl/epoint/mparr/categories/CategoriesController.java
+++ src/main/java/pl/epoint/mparr/categories/CategoriesController.java
@@ -21,31 +21,35 @@ class CategoriesController {
     @GetMapping
     ResponseEntity<CategoryDto> getCategory(@PathVariable("id") long id) {
         CategoryDto categoryDto = categoryService.findById(id);
-        if(categoryDto == null)
+        if(categoryDto == null) {
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
         return new ResponseEntity<>(categoryDto, HttpStatus.OK);
     }
 
     @PostMapping
     ResponseEntity<CategoryDto> addCategory(@RequestBody CategoryDto categoryDto) {
-        if(categoryService.categoryExists(categoryDto))
+        if(categoryService.categoryExists(categoryDto)) {
             return new ResponseEntity<>(categoryDto, HttpStatus.NOT_FOUND);
+        }
         categoryService.save(categoryDto);
         return new ResponseEntity<>(categoryDto, HttpStatus.CREATED);
     }
 
     @PutMapping
     ResponseEntity<CategoryDto> updateCategory(@RequestBody CategoryDto categoryDto, @PathVariable("id") long id) {
-        if(!categoryService.categoryExists(categoryDto))
+        if(!categoryService.categoryExists(categoryDto)) {
             return new ResponseEntity<>(categoryDto, HttpStatus.NOT_FOUND);
+        }
         categoryDto.setId(id);
         return new ResponseEntity<>(categoryDto, HttpStatus.OK);
     }
 
     @DeleteMapping
     ResponseEntity deleteCategory(@PathVariable("id") long id) {
-        if(categoryService.findById(id)==null)
+        if(categoryService.findById(id)==null) {
             return ResponseEntity.badRequest().build();
+        }
         categoryService.deleteById(id);
         return ResponseEntity.ok().build();
     }
